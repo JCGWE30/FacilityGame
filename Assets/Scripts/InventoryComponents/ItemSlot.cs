80c5cd6c93f2db2168b0c34c2c00259ba9f497d6
JC GWE <gwejc69@gmail.com>
2024-08-03 18:23:39 -0500

    ITEM SERILAZATION DONE FUCK YEA

--- Assets/Scripts/InventoryComponents/ItemSlot.cs
+++ Assets/Scripts/InventoryComponents/ItemSlot.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ItemSlot : MonoBehaviour
+public struct ItemSlotData : INetworkSerializable
+{
+    public int id;
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref id);
+    }
+}
+public class ItemSlot : MonoBehaviour,ISerializableComponent
 {
     public ItemDesc item { get; private set; }
     public SlotType slotType;
@@ -34,5 +43,22 @@ public class ItemSlot : MonoBehaviour
     public void ClearNextItem()
     {
         clearNext = true;
+    }
+
+    public INetworkSerializable SerializeComponent()
+    {
+        return new ItemSlotData
+        {
+            id = id
+        };
+    }
+
+    public void DeserializeComponent(INetworkSerializable serializedComponent)
+    {
+        ItemSlotData? data = serializedComponent as ItemSlotData?;
+        if (data.HasValue)
+        {
+            id = data.Value.id;
+        }
     }
 }
